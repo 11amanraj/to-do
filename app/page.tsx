c4104e3bba09f4754cbf640b78e2ea0a57cb2b81
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-10-10 20:03:39 +0530

    Added functionality to remove todos

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,11 @@ export default function Home() {
   ])
   const inputRef = useRef<HTMLInputElement>(null)
 
+  const todoDeleteHandler = (id: string) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+    setTodos([...updatedTodos])
+  }
+
   return (
     <main>
       <h1 className="text-3xl">To-Do App</h1>
@@ -30,14 +35,21 @@ export default function Home() {
               id: Math.random().toString(),
               text: todo,
               completed: false,
-            }]) 
+            }])
+            inputRef.current.value = '' 
           }
         }}>
         <input ref={inputRef} className="text-blue-900" type="text"/>
       </form>
 
       <div>
-        {todos.map(todo => <p key={todo.id}>{todo.text}</p>)}
+        {todos.map(todo => {
+          return (
+            <div key={todo.id} className="flex gap-4">
+              <p onClick={() => todoDeleteHandler(todo.id)}>Del</p>
+              <p>{todo.text}</p>
+            </div>)
+        })}
       </div>
     </main>
   )
