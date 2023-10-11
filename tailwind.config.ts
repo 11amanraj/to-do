147e2fdd8a8b9f199dc8398a438c58f6113a62d7
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-10-11 20:25:56 +0530

    Added color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,17 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'bgd': 'var(--bgd)',
+      'todo-box': 'var(--todo-box)',
+      'active-todo': 'var(--active-todo)',
+      'inactive-todo': 'var(--inactive-todo)',
+      'check-mark': 'var(--check-mark)',
+      'filter-active': 'var(--filter-active)',
+      'logo': 'var(--logo)',
+      'create-todo': 'var(--create-todo)',
+      'completed': 'var(--completed-gradient)',
+    }
   },
   plugins: [],
 }
